9a3529023177bea743bd9dba258597a3fef9b742
Tacial <18190041+Tacial@users.noreply.github.com>
2019-04-06 18:04:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <html>
- <head><link rel="stylesheet" type="text/css" href="style2.css"></head>
- <body></body>
+ <head>
+  <title>Sobre Mim</title>
+  <link rel="stylesheet" type="text/css" href="style2.css">
+ </head>
+ <body>
+  <header>
+  </header>
+ </body>
 </html>
